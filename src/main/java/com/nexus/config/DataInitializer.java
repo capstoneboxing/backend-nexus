b3a083561bc753acf91391421f89f66603a94968
tywaine <peterstywaine@gmail.com>
2026-03-23 10:57:13 -0500

    Updated All Time Ranked Boxer Controller and stuff

--- src/main/java/com/nexus/config/DataInitializer.java
+++ src/main/java/com/nexus/config/DataInitializer.java
@@ -2,6 +2,7 @@ package com.nexus.config;
 
 import com.nexus.model.Admin;
 import com.nexus.repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +12,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class DataInitializer {
 
+    @Value("${admin.default.password}")
+    private String rawPassword;
+
     @Bean
     CommandLineRunner init(AdminRepository adminRepository, PasswordEncoder passwordEncoder) {
         return args -> {
             if (adminRepository.count() == 0) {
-                String rawPassword = "password123";
                 String[] usernames = {"tywaine", "maurice", "vanessa", "karnardia"};
 
                 for (String username : usernames) {
